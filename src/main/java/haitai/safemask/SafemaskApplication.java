6c06a8892d6949819cb55ad1f8ca2e19e35acfd1
HongWangki <leejd8130@naver.com>
2026-07-01 14:07:19 +0900

    Init: spring boot project setup

---
+++ src/main/java/haitai/safemask/SafemaskApplication.java
@@ -0,0 +1,13 @@
+package haitai.safemask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafemaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafemaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haitai/safemask/SafemaskApplicationTests.java
@@ -0,0 +1,13 @@
+package haitai.safemask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafemaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
